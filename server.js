d2a5c4d161540345d3de61e0dbd8f08a7d1f797e
John Sanford <johnsanfordstudio@gmail.com>
2015-07-30 10:41:05 -0700

    created 404 page

--- server.js
+++ server.js
@@ -7,8 +7,11 @@ app.set('port', (process.env.PORT || 5000));
 app.use(express.static(__dirname + '/public'));
 
 app.get('/*', function (req, res) {
-  response.status(404).sendFile(__dirname + '/public/404.html')
+  res.status(404).sendFile(__dirname + '/public/404.html');
 });
+// app.get('/*', function (req, res) {
+//   res.status(404).send('woooo');
+// });
 app.listen(app.get('port'), function() {
   console.log('Node app is running on port', app.get('port'));
 });
